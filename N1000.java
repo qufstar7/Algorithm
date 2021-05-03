4df72efd350d4e6d580d542c85402c16b5a916ec
qufstar7 <57350342+qufstar7@users.noreply.github.com>
2021-05-03 16:08:58 +0900

    Add files via upload

---
+++ N1000.java
@@ -0,0 +1,16 @@
+package inputOutputAndFourRuleCalculataion;
+
+import java.util.Scanner;
+
+public class N1000 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A, B;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		System.out.println(A + B);
+
+	}
+
+}
